c9153946f8084b8d596368e9fa7ce5cf014704b3
Kuldeep Ghogre <kuldeep.ghogre@gmail.com>
2014-08-03 15:58:18 +0530

    Changing to single quote

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -8,7 +8,7 @@ describe Organization do
       expect(org.to_csv).to eq expected_result
     end
 
-    it "returns each repository's details as an array" do
+    it 'returns each repository\'s details as an array' do
       org = Organization.new('example', [{'name' => 'example_repo', 'language' => 'Ruby'},
                                          {'name' => 'example_repo_2', 'language' => 'Java'}])
       expected_result = [%w(example example_repo Ruby), %w(example example_repo_2 Java)]
