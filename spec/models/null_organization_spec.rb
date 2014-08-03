d76941e42ee3a7b5f20b7b8a26bbc6b4800fa422
Aarti <kriplaniaarti@gmail.com>
2014-08-03 14:13:14 +0530

    double to single quotes

--- spec/models/null_organization_spec.rb
+++ spec/models/null_organization_spec.rb
@@ -1,10 +1,10 @@
 require_relative '../../models/null_organization'
 
 describe NullOrganization do
-  describe "#to_csv" do
-    it "returns an empty array" do
+  describe '#to_csv' do
+    it 'returns an empty array' do
       org = NullOrganization.new
-      expected_result = [["","",""]]
+      expected_result = [['', '', '']]
       expect(org.to_csv).to eq expected_result
     end
   end
